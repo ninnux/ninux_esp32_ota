8d8f77c6d179c422840c7b78af6c7de9de7a166d
Nino <nino@ninux.org>
2019-05-14 22:51:21 +0200

    non gira da debuggare

--- include/ninux_esp32_ota.h
+++ include/ninux_esp32_ota.h
@@ -14,6 +14,20 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 
+
+// da ota_task
+#include "string.h"
+#include "esp_flash_partitions.h"
+#include "esp_partition.h"
+#define BUFFSIZE 1024
+static char ota_write_data[BUFFSIZE + 1] = { 0 };
+static void http_cleanup(esp_http_client_handle_t client);
+//=========
+
+// per http req
+#include "esp_tls.h"
+//=====
+
 static const char *TAG = "ninux_esp32_ota";
 extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
@@ -22,6 +36,10 @@ extern EventGroupHandle_t wifi_event_group;
 extern const int CONNECTED_BIT;
 
 
+//static void https_get_url(char *url, char *response);
+static void https_get_url(char *url);
+
 esp_err_t _http_event_handler(esp_http_client_event_t *evt);
 void simple_ota_example_task(void * pvParameter);
+
 void ninux_esp32_ota();
--- ninux_esp32_ota.c
+++ ninux_esp32_ota.c
@@ -30,6 +30,97 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 }
 
 
+
+//static void https_get_url(char *url, char *response)
+static void https_get_url(char *url)
+{
+    char buf[512];
+    int ret, len;
+
+    esp_tls_cfg_t cfg = {
+        .cacert_pem_buf  = server_cert_pem_start,
+        .cacert_pem_bytes = server_cert_pem_end - server_cert_pem_start,
+    };
+    
+    ESP_LOGI(TAG, "pippo1");
+    struct esp_tls *tls = esp_tls_conn_http_new(url, &cfg);
+    
+    ESP_LOGI(TAG, "pippo2");
+    if(tls != NULL) {
+        ESP_LOGI(TAG, "Connection established...");
+    } else {
+        ESP_LOGE(TAG, "Connection failed...");
+        goto exit;
+    }
+    
+    ESP_LOGI(TAG, "pippo3");
+    size_t written_bytes = 0;
+    char request[1024];
+    ESP_LOGI(TAG, "pippo4");
+    //sprintf(request,"GET %s HTTP/1.0\r\n"
+    //     "Host: iotfw.ninux.org\r\n"
+    //     "User-Agent: esp-idf/1.0 esp32\r\n"
+    //     "\r\n",url);
+    sprintf(request,"GET %s HTTP/1.0\r\n",url);
+    ESP_LOGI(TAG, "pippo5");
+    do {
+        ret = esp_tls_conn_write(tls, request + written_bytes, strlen(request) - written_bytes);
+        if (ret >= 0) {
+            ESP_LOGI(TAG, "%d bytes written", ret);
+            written_bytes += ret;
+        } else if (ret != MBEDTLS_ERR_SSL_WANT_READ  && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+            ESP_LOGE(TAG, "esp_tls_conn_write  returned 0x%x", ret);
+            goto exit;
+        }
+    } while(written_bytes < strlen(request));
+
+    ESP_LOGI(TAG, "Reading HTTP response...");
+
+    do
+    {
+        len = sizeof(buf) - 1;
+        bzero(buf, sizeof(buf));
+        ret = esp_tls_conn_read(tls, (char *)buf, len);
+        
+        if(ret == MBEDTLS_ERR_SSL_WANT_WRITE  || ret == MBEDTLS_ERR_SSL_WANT_READ)
+            continue;
+        
+        if(ret < 0)
+       {
+            ESP_LOGE(TAG, "esp_tls_conn_read  returned -0x%x", -ret);
+            break;
+        }
+
+        if(ret == 0)
+        {
+            ESP_LOGI(TAG, "connection closed");
+            break;
+        }
+
+        len = ret;
+        ESP_LOGD(TAG, "%d bytes read", len);
+        /* Print response directly to stdout as it is read */
+        for(int i = 0; i < len; i++) {
+            putchar(buf[i]);
+	    //sprintf((response+i),"%c",buf[i]);
+        }
+    } while(1);
+
+    exit:
+        esp_tls_conn_delete(tls);    
+        putchar('\n'); // JSON output doesn't have a newline at end
+	//sprintf(response+strlen(response),"\n");
+	
+        static int request_count;
+        ESP_LOGI(TAG, "Completed %d requests", ++request_count);
+	//return response;
+	//strncpy(response,buf,sizeof(buf));
+        //bzero(response, strlen(response));
+	//sprintf(response,"prova:%s",buf);
+
+}
+
+
 void simple_ota_example_task(void * pvParameter)
 {
     ESP_LOGI(TAG, "Starting OTA example");
@@ -42,6 +133,52 @@ void simple_ota_example_task(void * pvParameter)
 
 
     /* ASK FOR FIRMWARE URL */
+    
+    esp_http_client_config_t config = {
+        .url = CONFIG_FIRMWARE_UPGRADE_URL,
+        .cert_pem = (char *)server_cert_pem_start,
+        .event_handler = _http_event_handler,
+    };
+    esp_err_t ret = esp_https_ota(&config);
+    if (ret == ESP_OK) {
+        esp_restart();
+    } else {
+        ESP_LOGE(TAG, "Firmware upgrade failed");
+    }
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+void simple_ota_version_task(void * pvParameter)
+{
+    ESP_LOGI(TAG, "Starting OTA version check");
+
+    const esp_partition_t *running = esp_ota_get_running_partition();
+
+    /* Wait for the callback to set the CONNECTED_BIT in the
+       event group.
+    */
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to WiFi network! Attempting to connect to server...");
+
+    /* ASK FOR FIRMWARE URL */
+    //strncpy(fw_url,https_get_url("https://iotfw.ninux.org/firwmare_check"),4096);
+    //char fw_url[4096]; 
+    //https_get_url("https://iotfw.ninux.org/firwmare_check",fw_url);
+    //https_get_url("https://10.162.0.77/firwmare_check",fw_url);
+    //ESP_LOGI(TAG, "To be downloaded:%s\n",fw_url);
+    https_get_url("https://iotfw.ninux.org/firwmare_check");
+    //https_get_url("https://10.162.0.77/firwmare_check");
+
+    /* version check */
+    ESP_LOGI(TAG, "Running partition type %d subtype %d (offset 0x%08x)",
+             running->type, running->subtype, running->address);
+    esp_app_desc_t running_app_info;
+    if (esp_ota_get_partition_description(running, &running_app_info) == ESP_OK) {
+        ESP_LOGI(TAG, "Running firmware version: %s", running_app_info.version);
+    }
     
     esp_http_client_config_t config = {
         .url = CONFIG_FIRMWARE_UPGRADE_URL,
@@ -74,5 +211,6 @@ void ninux_esp32_ota()
     }
     ESP_ERROR_CHECK( err );
 
-    xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+    //xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&simple_ota_version_task, "ota_version_task", 8192, NULL, 5, NULL);
 }
